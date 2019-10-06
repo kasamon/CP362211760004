b99188d60cf5074542903b099f017bf4dd4a3c53
kasamon okong <21315@mns.ac.th>
2019-10-06 14:02:39 +0700

    lab7

---
+++ src/lab7/ex5.java
@@ -0,0 +1,24 @@
+package lab7;
+
+import java.util.Scanner;
+
+public class ex5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int totalEven = 0;
+        int totalOdd = 0;
+        int x = 0;
+        for (int i = 0; i < 10; i++) {
+
+            System.out.print("Enter an integer");
+            x = sc.nextInt();
+            if (x % 2 == 0)
+                totalEven +=x;
+            else
+                totalOdd +=x;
+            }
+            System.out.print("Total of even number" + totalEven);
+            System.out.print("Total of Odd number" + totalOdd);
+        }
+    }
