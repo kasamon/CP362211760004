00ef412e2f1aa02a3980177672d75b21e534e368
kasamon okong <21315@mns.ac.th>
2019-10-06 11:24:49 +0700

    lab7

---
+++ src/lab7/ex3.java
@@ -0,0 +1,46 @@
+package lab7;
+
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Which subjects you like the most?:");
+        System.out.println("1.System Analysis and Design:");
+        System.out.println("2.Computer Programming:");
+        System.out.println("3.Internet Application Programming:");
+        System.out.println("Select:");
+        int selector = sc.nextInt();
+
+        switch (selector){
+            case  1: System.out.println("Sure? This subject is very hard.");break;
+            case  2: System.out.println("Yes, Computer Programming is very fun.");break;
+            case  3: System.out.println("ohhh, you might expert in programming.");break;
+            default: System.out.println("You have to enter 1-3.");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/lab7/ex4.java
@@ -0,0 +1,26 @@
+package lab7;
+
+public class ex4 {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0){
+                System.out.print("Hello.");
+                System.out.println("KOY.");
+        }else
+            System.out.println("Hello.");
+
+
+            }
+
+
+    }
+
+
+
+
+
+
+
+}
